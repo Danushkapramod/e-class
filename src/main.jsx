2ca2a16c41e909137ee8709e8c75e2322ef0ebf6
Danushka <k.danushkapramod@gmail.com>
2025-08-20 13:17:27 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import Index from './pages/Index.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-  <>
-    <App />
-    <Index />
-  </>
-  // </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
